6dc1071042e93bf1947eff2897c7ed309084c365
kimfy <kim@wwan.no>
2022-12-30 12:26:42 +0100

    attempt at fixing the navigation bar at the top

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
-
+<div class="container">
 <nav>
-  <a href="/">Home</a>
+  <a href="/">KIM</a>
+  <ul>
+    <li><a href="/">HOME</a></li>
+    <li><a href="/posts">POSTS</a></li>
+  </ul>
 </nav>
+</div>
 
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
@@ -13,6 +18,42 @@
   :global(body) {
     background-color: #16161e;
   }
+
+  .container {
+    display: flex;
+    justify-content: center;
+    min-width: 40em;
+  }
+
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 40em;
+    max-width: 40em;
+  }
+
+  a {
+    link-style: none;
+    text-decoration: none;
+    color: white;
+    border-top: 3px solid white;
+    border-bottom: 3px solid white;
+    padding-top: 10px;
+    padding-bottom: 10px;
+  }
+
+  a:hover {
+    border-top: 5px solid white;
+    border-bottom: 5px solid white;
+  }
+
+  nav > ul {
+    display: flex;
+    gap: 30px;
+    list-style: none;
+  }
+
 </style>
 
 <slot></slot>
